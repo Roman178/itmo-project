a32927a2a89f71c030d41e637fd2538352ab9559
Roman <ismagilov771@gmail.com>
2021-08-02 02:27:44 +0300

    fix: fixing scripts

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-// Карусель
-const prevBtn = document.querySelector(".carousel__btn_type_prev");
-const nextBtn = document.querySelector(".carousel__btn_type_next");
-const carousel = document.querySelector(".carousel__cards-box");
-const dotsBox = document.querySelector(".carousel__dots");
+// Карусель intro
+const intro = document.querySelector(".intro");
+
+const prevBtn = intro.querySelector(".carousel__btn_type_prev");
+const nextBtn = intro.querySelector(".carousel__btn_type_next");
+const carousel = intro.querySelector(".carousel__cards-box");
+const dotsBox = intro.querySelector(".carousel__dots");
 const dots = dotsBox.querySelectorAll(".carousel__dot");
 
 function switchCarouselSlide(slides) {
@@ -100,5 +102,77 @@ prevBtn.addEventListener("click", () => {
 dots.forEach((el, i) => {
   el.addEventListener("click", () => {
     switchSlideByDot(i + 1, carousel, el);
+  });
+});
+
+// Карусель team (must fix)
+const teamSection = document.querySelector(".team");
+
+const prevBtnTeam = teamSection.querySelector(".carousel__btn_type_prev");
+const nextBtnTeam = teamSection.querySelector(".carousel__btn_type_next");
+const carouselTeam = teamSection.querySelector(".carousel__cards-box");
+const dotsBoxTeam = teamSection.querySelector(".carousel__dots");
+const dotsTeam = dotsBoxTeam.querySelectorAll(".carousel__dot");
+
+function switchCarouselSlideTeam(slides) {
+  dotsTeam.forEach((el) => el.classList.remove("carousel__dot_active"));
+  if (
+    carouselTeam.classList.contains(
+      `carousel__cards-box_slide_${slides.a.nameSlide}`
+    )
+  ) {
+    carouselTeam.classList.remove(
+      `carousel__cards-box_slide_${slides.a.nameSlide}`
+    );
+    carouselTeam.classList.add(
+      `carousel__cards-box_slide_${slides.b.nameSlide}`
+    );
+    dotsTeam[slides.b.numDot - 1].classList.add("carousel__dot_active");
+  } else {
+    dotsTeam[slides.b.numDot - 1].classList.add("carousel__dot_active");
+    return;
+  }
+}
+
+function switchSlideByDotTeam(dotIndex, carouselEl, dotEl) {
+  carouselEl.className = "";
+  dotsTeam.forEach((el) => el.classList.remove("carousel__dot_active"));
+  dotEl.classList.add("carousel__dot_active");
+  switch (dotIndex) {
+    case 1:
+      carouselEl.classList.add(
+        "carousel__cards-box",
+        "carousel__cards-box_slide_first"
+      );
+      break;
+    case 2:
+      carouselEl.classList.add(
+        "carousel__cards-box",
+        "carousel__cards-box_slide_second"
+      );
+      break;
+    default:
+      carouselEl.classList.add("carousel__cards-box");
+      break;
+  }
+}
+
+nextBtnTeam.addEventListener("click", () => {
+  switchCarouselSlideTeam({
+    a: { nameSlide: "first", numDot: 1 },
+    b: { nameSlide: "second", numDot: 2 },
+  });
+});
+
+prevBtnTeam.addEventListener("click", () => {
+  switchCarouselSlideTeam({
+    a: { nameSlide: "second", numDot: 2 },
+    b: { nameSlide: "first", numDot: 1 },
+  });
+});
+
+dotsTeam.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    switchSlideByDotTeam(i + 1, carouselTeam, el);
   });
 });
